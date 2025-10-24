ed072f408c9d28c3d58a87e4654857059958cc65
igorkrastev980 <igor.krastev@gmail.com>
2025-10-24 12:03:12 +0200

    Update create-checkout-session.js

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       console.log("Stripe key starts with:", process.env.STRIPE_SECRET_KEY?.substring(0, 10));
-      const session = await stripe.checkout.sessions.create({
+      const session = await console.log("Stripe key starts with:", process.env.STRIPE_SECRET_KEY?.substring(0, 10));stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         line_items: [
           {
