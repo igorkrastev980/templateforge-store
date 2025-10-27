13ed6e3e5d584fcff520b2bf7d020014b2dd435a
igorkrastev980 <igor.krastev@gmail.com>
2025-10-27 20:03:42 +0100

    git add pages/api/debug-env.js git commit -m "Add debug endpoint for Stripe keys" git push

--- pages/api/debug-env.js
+++ pages/api/debug-env.js
@@ -1,1 +1,10 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    stripeKeyExists: !!process.env.STRIPE_SECRET_KEY,
+    keyStart: process.env.STRIPE_SECRET_KEY?.slice(0, 8),
+    keyEnd: process.env.STRIPE_SECRET_KEY?.slice(-6),
+    publishableKeyExists: !!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    pubKeyStart: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY?.slice(0, 8),
+    pubKeyEnd: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY?.slice(-6),
+  });
+}
