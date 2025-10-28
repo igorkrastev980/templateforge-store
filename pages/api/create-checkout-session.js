c14bf4e792074c754954187f755bd70508cd635a
igorkrastev980 <igor.krastev@gmail.com>
2025-10-28 20:23:21 +0100

    Update create-checkout-session.js

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
           },
         ],
         success_url: `${req.headers.origin}/success`,
+        session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/cancel`,
       });
 
